7fe80d5dc5f90e75979c54c3385335a3c9d2bb19
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2026-07-28 21:59:16 -0700

    Finish exercises 6-9

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterString, setFilterString] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (persons.some((person) => newName === person.name)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    if (persons.some((person) => newNumber === person.number)) {
+      alert(`${newNumber} is already added to phonebook`);
+      return;
+    }
+
+    setPersons([...persons, { name: newName, number: newNumber }]);
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterStringChange = (event) => {
+    setFilterString(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      filter shown with
+      <input value={filterString} onChange={handleFilterStringChange} />
+      <h2>Add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons
+          .filter((person) =>
+            person.name.toLowerCase().includes(filterString.toLowerCase()),
+          )
+          .map((person) => (
+            <li key={person.id}>
+              {person.name} {person.number}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
